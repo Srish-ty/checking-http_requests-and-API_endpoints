54ec8ece84bab7a3b9a4eb3ce1fa59fcb18034e6
Srishty Mangutte <srishtymangutte@gmail.com>
2023-06-15 01:17:43 +0530

    feat: add some more files

---
+++ yt_comment-post-request1.js
@@ -0,0 +1,33 @@
+fetch("https://www.youtube.com/youtubei/v1/comment/create_comment?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "authorization": "SAPISIDHASH 1686768029_dfc93c547f44cca06e7a67126e76ac13d7106cc4",
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+    "sec-ch-ua-arch": "\"x86\"",
+    "sec-ch-ua-bitness": "\"64\"",
+    "sec-ch-ua-full-version": "\"114.0.5735.110\"",
+    "sec-ch-ua-full-version-list": "\"Not.A/Brand\";v=\"8.0.0.0\", \"Chromium\";v=\"114.0.5735.110\", \"Google Chrome\";v=\"114.0.5735.110\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-model": "\"\"",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-ua-platform-version": "\"15.0.0\"",
+    "sec-ch-ua-wow64": "?0",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "same-origin",
+    "sec-fetch-site": "same-origin",
+    "x-client-data": "CIS2yQEIpbbJAQipncoBCLTgygEIlaHLAQjosswBCJ2czQEIhaDNAQiOp80BCMGqzQEItrLNAQjss80BCNq0zQEInbnNAQ==",
+    "x-goog-authuser": "0",
+    "x-goog-visitor-id": "Cgt5aUZGSjlDX2t5YyiAkqikBg%3D%3D",
+    "x-origin": "https://www.youtube.com",
+    "x-youtube-bootstrap-logged-in": "true",
+    "x-youtube-client-name": "1",
+    "x-youtube-client-version": "2.20230607.06.00",
+    "cookie": "VISITOR_INFO1_LIVE=yiFFJ9C_kyc; YSC=jFxBbhBDFRg; LOGIN_INFO=AFmmF2swRAIgMqFCHQMCutCKs0fzrhrYAMQebDhtHxsrXmpdyslA8JsCIBNpk5Z8cNz2iOFz99VHWk7fzIpqy7uvJ6JUcAGTw_kS:QUQ3MjNmd0ZOVjZyZ2oxVk5xT2o5UUpCYnV3cnAwU3dRazJKajkwbGJzT29BVVpTSkRzZjgyXzhvRDh4NXp6T0Y4WjViTTBpSk53WENxX1BSZ2FtWXhjU0lobGhZMVh0VGxKR1VFREQwNmRsZjMtN2k1RG9rV1EwV3BtWXdzOS1adDU2QkxMX1NJcnRGNVY4R0owbVVOQlZIdTVJeU44bDh3; PREF=f4=4000000&f6=40000000&tz=Asia.Calcutta&f5=30000&f7=100; DEVICE_INFO=ChxOekU0TmprME9EWTVOekl3TXpNM05qWTJNUT09EMPX9J0GGMPX9J0G; SID=Wwj4-SE1NJxRMZo21Xcb7jfx4Tl4i5AhJhjyx-asxAS9u9O3eWqrnKbr58sA2M_66YPkFA.; __Secure-1PSID=Wwj4-SE1NJxRMZo21Xcb7jfx4Tl4i5AhJhjyx-asxAS9u9O3iFqP2J9eF_TvhnOomvh_Ew.; __Secure-3PSID=Wwj4-SE1NJxRMZo21Xcb7jfx4Tl4i5AhJhjyx-asxAS9u9O32qKzqBGsqGZwdpVHR0VMgQ.; HSID=APdjxK6jkDE93umMl; SSID=A-co6a28xypvzuiyg; APISID=x1DXKXJcet2fI7P0/Ar7Yvb-1e9vLzJvR1; SAPISID=fRgpicL2Pb5_bxV9/AuquS_EFROrBHG0-w; __Secure-1PAPISID=fRgpicL2Pb5_bxV9/AuquS_EFROrBHG0-w; __Secure-3PAPISID=fRgpicL2Pb5_bxV9/AuquS_EFROrBHG0-w; CONSISTENCY=ACeCFAX3nT75ThTcewsUvGwyCoEHoKs2lsayWTUfAbIgky6hHFgEi5VtP-e8cbDhP7zNQ3MJBD16bKTV08bxHm_3AmCJWQDaJ31qfhzQLm9alGMQFqLBswVdiuTO4VEeA64EP2XXH4RBnMNnMPrq7PqkX5_Zt8t8fZHqyAlsq2QYJOzdaYU-O-qdKI8F3x5rFG40XP7pDSibNGrg; SIDCC=AP8dLtxo5A2MG3blKjkLAXhnwI11GzuXV-OJ36eLvAaTU8twwP0kUea7OLDjUYGLZZGnFByV224; __Secure-1PSIDCC=AP8dLtw2AAiFg9mCFU2Zi-IkPAGbDQnlXoiR-9msibHtXmkLOM7Y0obZpeHLF3d4AYYXWWRxAw; __Secure-3PSIDCC=AP8dLtzLK4qytDwS6EwRHdCid3P39Xu7c1KfQiFTj_teIiU7mZuiL5saePdQP243a-7N1yKarA; ST-1hfk6tr=",
+    "Referer": "https://www.youtube.com/watch?v=v2X51AVgl3o",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"IN\",\"remoteHost\":\"118.185.231.8\",\"deviceMake\":\"\",\"deviceModel\":\"\",\"visitorData\":\"Cgt5aUZGSjlDX2t5YyiAkqikBg%3D%3D\",\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36,gzip(gfe)\",\"clientName\":\"WEB\",\"clientVersion\":\"2.20230607.06.00\",\"osName\":\"Windows\",\"osVersion\":\"10.0\",\"originalUrl\":\"https://www.youtube.com/\",\"screenPixelDensity\":1,\"platform\":\"DESKTOP\",\"clientFormFactor\":\"UNKNOWN_FORM_FACTOR\",\"configInfo\":{\"appInstallData\":\"CICSqKQGEPi1rwUQzK7-EhCqsv4SEO6irwUQouyuBRCQo68FEKK0rwUQgp2vBRClma8FENuvrwUQvbauBRDyqK8FEMO3_hIQ_rWvBRDrk64FEKXC_hIQuIuuBRDUoa8FEOSz_hIQzLf-EhCMt68FEOCnrwUQ57qvBRDk8q4FEOf3rgUQzN-uBRDi1K4FEInorgUQurSvBRDVtq8FEOnDrwU%3D\"},\"screenDensityFloat\":1.25,\"userInterfaceTheme\":\"USER_INTERFACE_THEME_DARK\",\"timeZone\":\"Asia/Calcutta\",\"browserName\":\"Chrome\",\"browserVersion\":\"114.0.0.0\",\"acceptHeader\":\"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\",\"deviceExperimentId\":\"ChxOekU0TmprME9EWTVOekl3TXpNM05qWTJNUT09EICSqKQGGMPX9J0G\",\"screenWidthPoints\":747,\"screenHeightPoints\":746,\"utcOffsetMinutes\":330,\"connectionType\":\"CONN_CELLULAR_4G\",\"memoryTotalKbytes\":\"8000000\",\"mainAppWebInfo\":{\"graftUrl\":\"https://www.youtube.com/watch?v=v2X51AVgl3o\",\"pwaInstallabilityStatus\":\"PWA_INSTALLABILITY_STATUS_CAN_BE_INSTALLED\",\"webDisplayMode\":\"WEB_DISPLAY_MODE_BROWSER\",\"isWebNativeShareAvailable\":true}},\"user\":{\"lockedSafetyMode\":false},\"request\":{\"useSsl\":true,\"internalExperimentFlags\":[],\"consistencyTokenJars\":[{\"encryptedTokenJarContents\":\"ACeCFAX3nT75ThTcewsUvGwyCoEHoKs2lsayWTUfAbIgky6hHFgEi5VtP-e8cbDhP7zNQ3MJBD16bKTV08bxHm_3AmCJWQDaJ31qfhzQLm9alGMQFqLBswVdiuTO4VEeA64EP2XXH4RBnMNnMPrq7PqkX5_Zt8t8fZHqyAlsq2QYJOzdaYU-O-qdKI8F3x5rFG40XP7pDSibNGrg\",\"expirationSeconds\":\"600\"}]},\"clientScreenNonce\":\"MC4xMTEyODc5MzI2ODkyMDI2Nw..\",\"clickTracking\":{\"clickTrackingParams\":\"CO0DEPBbIhMItfXur7TD_wIVwZhwCh1wJAXj\"},\"adSignalsInfo\":{\"params\":[{\"key\":\"dt\",\"value\":\"1686767874971\"},{\"key\":\"flash\",\"value\":\"0\"},{\"key\":\"frm\",\"value\":\"0\"},{\"key\":\"u_tz\",\"value\":\"330\"},{\"key\":\"u_his\",\"value\":\"3\"},{\"key\":\"u_h\",\"value\":\"864\"},{\"key\":\"u_w\",\"value\":\"1536\"},{\"key\":\"u_ah\",\"value\":\"816\"},{\"key\":\"u_aw\",\"value\":\"1536\"},{\"key\":\"u_cd\",\"value\":\"24\"},{\"key\":\"bc\",\"value\":\"31\"},{\"key\":\"bih\",\"value\":\"729\"},{\"key\":\"biw\",\"value\":\"731\"},{\"key\":\"brdim\",\"value\":\"0,0,0,0,1536,0,1536,816,747,746\"},{\"key\":\"vis\",\"value\":\"1\"},{\"key\":\"wgl\",\"value\":\"true\"},{\"key\":\"ca_type\",\"value\":\"image\"}],\"bid\":\"ANyPxKpEGU0Syvhq9S7M_efrgqZ5xn9rnF-ORr6qfTaL1wNuSaLlXGR4EgEgFRvfCZYsaFfgkeNlZpAx8HXAq_hT8TGo3yFrqQ\"}},\"createCommentParams\":\"Egt2Mlg1MUFWZ2wzbyoCCABQBw%3D%3D\",\"commentText\":\"opensource is the redemption for us now\"}",
+  "method": "POST"
+});
